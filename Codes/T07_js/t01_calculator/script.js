4ca93bd79fe0e5287bbca9137471d32ede148494
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-12-08 06:47:20 +0700

    update calculator

--- Codes/T07_js/t01_calculator/script.js
+++ Codes/T07_js/t01_calculator/script.js
@@ -7,12 +7,14 @@ const b2 = document.querySelector('#b2');
 const b3 = document.querySelector('#b3');
 const bPlus = document.querySelector('#plus');
 const bEqual = document.querySelector('#equal');
+const bDelete = document.querySelector('#delete');
 const bReset = document.querySelector('#reset');
 b1.addEventListener('click', click1);
 b2.addEventListener('click', click2);
 b3.addEventListener('click', click3);
 bPlus.addEventListener('click', clickPlus);
 bEqual.addEventListener('click', clickEqual);
+bDelete.addEventListener('click', clickDelete);
 bReset.addEventListener('click', clickReset);
 
 function click1() {
@@ -32,12 +34,22 @@ function clickPlus() {
   input.textContent = inputText;
 }
 function clickEqual() {
-  const evalResult = eval(inputText);
-  result.textContent = evalResult;
+  try {
+    const evalResult = eval(inputText);
+    result.textContent = evalResult;
+  } catch {
+    result.textContent = 'NaN';
+  }
+}
+
+function clickDelete() {
+  inputText = inputText.slice(0, -1);
+  input.textContent = inputText;
+  result.textContent = '';
 }
 
 function clickReset() {
   inputText = '';
-  input.textContent = '';
+  input.textContent = inputText;
   result.textContent = '';
 }
