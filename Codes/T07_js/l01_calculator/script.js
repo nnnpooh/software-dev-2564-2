2583dc479cc5c290a80cf8b3bf3efbfb0cded2ba
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2021-12-14 10:49:20 +0700

    Update calculator and lecture

---
+++ Codes/T07_js/l01_calculator/script.js
@@ -0,0 +1,55 @@
+let inputText = '';
+
+const input = document.querySelector('.input');
+const result = document.querySelector('.result');
+
+const b1 = document.querySelector('#b1');
+const b2 = document.querySelector('#b2');
+const b3 = document.querySelector('#b3');
+const bPlus = document.querySelector('#plus');
+const bEqual = document.querySelector('#equal');
+const bDelete = document.querySelector('#delete');
+const bReset = document.querySelector('#reset');
+
+b1.addEventListener('click', click1);
+b2.addEventListener('click', click2);
+b3.addEventListener('click', click3);
+bPlus.addEventListener('click', clickPlus);
+bEqual.addEventListener('click', clickEqual);
+bReset.addEventListener('click', clickReset);
+bDelete.addEventListener('click', clickDelete);
+
+function click1() {
+  inputText += '1';
+  input.textContent = inputText;
+}
+function click2() {
+  inputText += '2';
+  input.textContent = inputText;
+}
+function click3() {
+  inputText += '3';
+  input.textContent = inputText;
+}
+function clickPlus() {
+  inputText += '+';
+  input.textContent = inputText;
+}
+function clickEqual() {
+  try {
+    const evalResult = eval(inputText);
+    result.textContent = evalResult;
+  } catch {
+    result.textContent = 'NaN';
+  }
+}
+function clickReset() {
+  inputText = '';
+  input.textContent = inputText;
+  result.textContent = inputText;
+}
+function clickDelete() {
+  inputText = inputText.slice(0, -1);
+  input.textContent = inputText;
+  result.textContent = '';
+}
