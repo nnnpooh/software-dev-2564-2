f9347194ea9a18792b9a27517c6e6b6a2fbcd947
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-12-16 09:28:03 +0700

    update js

--- Codes/T08_js/t01_calculator/script.js
+++ Codes/T08_js/t01_calculator/script.js
@@ -1,24 +1,45 @@
 let inputText = '';
+const btnRegularMap = {
+  b1: '1',
+  b2: '2',
+  b3: '3',
+  b4: '4',
+  b5: '5',
+  b6: '6',
+  b7: '7',
+  b8: '8',
+  b9: '9',
+  b0: '0',
+  'b-plus': '+',
+  'b-minus': '-',
+  'b-multiply': '*',
+  'b-divide': '/',
+};
+
+const btnCommandMap = {
+  'b-equal': clickEqual,
+  'b-delete': clickDelete,
+  'b-reset': clickReset,
+};
+
 const input = document.querySelector('.input');
 const result = document.querySelector('.result');
+const btnNumOpArray = document.querySelectorAll('.btn.regular');
+const btnCommandArray = document.querySelectorAll('.btn.command');
 
-const bEqual = document.querySelector('#equal');
-const bDelete = document.querySelector('#delete');
-const bReset = document.querySelector('#reset');
-const btnArray = document.querySelectorAll('.btn.num-op');
-
-for (const btn of btnArray) {
+for (const btn of btnNumOpArray) {
   const id = btn.getAttribute('id');
   btn.addEventListener('click', handleClick);
 }
 
-bEqual.addEventListener('click', clickEqual);
-bDelete.addEventListener('click', clickDelete);
-bReset.addEventListener('click', clickReset);
+for (const btn of btnCommandArray) {
+  const id = btn.getAttribute('id');
+  btn.addEventListener('click', btnCommandMap[id]);
+}
 
 function handleClick(e) {
   const id = e.target.getAttribute('id');
-  inputText += id.slice(1);
+  inputText += btnRegularMap[id];
   input.textContent = inputText;
 }
 
