ebdff42d2400fd47223e862e402c75bd7cc7987a
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2021-12-20 21:07:55 +0700

    update js

--- Lecture/src/T09_js/playground.js
+++ Lecture/src/T09_js/playground.js
@@ -35,11 +35,67 @@
 
 // myFunction()
 
-let carName = 'Volvo';
+// let carName = 'Volvo';
 
-function myFunction() {
-  let carName = 'Honda';
-  console.log(carName);
-}
+// function myFunction() {
+//   let carName = 'Honda';
+//   console.log(carName);
+// }
 
-console.log(carName);
+// console.log(carName);
+
+// let choice = 'A'; // 'A', 'B', 'C'
+
+// if (choice === 'A') {
+//   console.log('You chose A.');
+// } else if (choice === 'B') {
+//   console.log('You chose B.');
+// } else if (choice === 'C') {
+//   console.log('You chose C.');
+// } else {
+//   console.log('Unknown Choice');
+// }
+
+// function absolute(number) {
+//   if (!number) return -1; // Error code
+//   return Math.abs(number);
+// }
+
+// console.log(absolute(-2));
+// console.log(absolute(null));
+// console.log(absolute(undefined));
+
+// let choice = 'A';
+// let score;
+// switch (choice) {
+//   case 'A':
+//     score = 10;
+//     break;
+//   case 'B':
+//     score = 5;
+//     break;
+//   case 'C':
+//     score = 1;
+//     break;
+//   default:
+//     score = 0;
+//     break;
+// }
+
+// console.log(score);
+
+// let choice = 'A';
+
+// let mapping = {
+//   A: 10,
+//   B: 5,
+//   C: 1,
+// };
+
+// result = mapping[choice] ? mapping[choice] : 0;
+
+// console.log(result);
+
+let loading = true;
+
+loading ? console.log('Loading...') : console.log('Done!');
