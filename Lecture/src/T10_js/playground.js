8bbe0d842e22516a12fb1cb1671693d0c4b7bea2
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-12-21 16:19:16 +0700

    Update

---
+++ Lecture/src/T10_js/playground.js
@@ -0,0 +1,19 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log(Math.random());
+// }
+
+// const person = {
+//   age: 20,
+//   weight: 60,
+//   height: 170,
+// };
+
+// for (const entry of Object.entries(person)) {
+//   console.log(entry);
+// }
+
+const numbers = [4, 11, 42, 14, 39];
+
+const numberFilter = numbers.find((item) => item > 12);
+
+console.log(numberFilter);
