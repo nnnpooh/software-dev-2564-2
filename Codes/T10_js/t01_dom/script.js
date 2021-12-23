787b253f0571219d2066acf8b70983d457d46733
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-12-23 14:39:59 +0700

    update

---
+++ Codes/T10_js/t01_dom/script.js
@@ -0,0 +1,11 @@
+const html = document.childNodes[1];
+console.log(html);
+
+const head = html.childNodes[0];
+const body = html.childNodes[2];
+
+console.log(head);
+console.log(body);
+
+const div1 = body.childNodes[3];
+console.log(div1);
--- Lecture/src/T10_js/playground.js
+++ Lecture/src/T10_js/playground.js
@@ -49,14 +49,67 @@
 
 // console.log(a);
 
-let a = [1, 2, 3];
+// let a = [1, 2, 3];
 
-function myFunc(a) {
-  let b = a.slice(0);
-  b.push(4);
-  console.log(b);
-}
+// function myFunc(a) {
+//   let b = a.slice(0);
+//   b.push(4);
+//   console.log(b);
+// }
 
-myFunc(a);
+// myFunc(a);
 
-console.log(a);
+// console.log(a);
+
+// const names = ['Taylor', 'Donald', 'Don', 'Natasha', 'Bobby'];
+
+// const nameModified = names.map((name) => 'This is ' + name + '.');
+
+// console.log(nameModified)
+
+// const note = {
+//   id: 1,
+//   title: 'My first note',
+//   date: '01/01/1970',
+// };
+// // Destructure properties into variables
+// const { id, title, date } = note;
+
+// console.log(id);
+// console.log(title);
+// console.log(date);
+
+// const tools = ['hammer', 'screwdriver'];
+// const otherTools = ['wrench', 'saw'];
+
+// // Concatenate tools and otherTools together
+// const allTools1 = tools.concat(otherTools);
+
+// // Unpack the tools Array into the allTools Array
+// const allTools2 = [...tools, ...otherTools];
+
+// console.log(allTools1, allTools2);
+
+// const user = {
+//   id: 3,
+//   name: 'Ron',
+// };
+// const updatedUser = { ...user, isLoggedIn: true };
+
+// console.log(updatedUser);
+
+// function multiply(a, b, c) {
+//   return a * b * c;
+// }
+
+// const numbers = [1, 2, 3];
+
+// console.log(multiply(...numbers));
+
+// function restTest(...args) {
+//   console.log(args);
+// }
+
+// restTest(1, 2, 3, 4, 5, 6);
+
+a = { a: 1, b: 2 };
