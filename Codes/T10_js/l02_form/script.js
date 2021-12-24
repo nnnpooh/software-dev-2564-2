f245e6626771e4f84112ac1913b2a5f64e828be5
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-12-24 23:40:15 +0700

    Update form validation

--- Codes/T10_js/l02_form/script.js
+++ Codes/T10_js/l02_form/script.js
@@ -3,12 +3,13 @@ const btnElement = document.getElementById('btn');
 const inputArray = document.querySelectorAll('input');
 
 form.addEventListener('submit', logSubmit);
+
 for (const input of inputArray) {
   const id = input.getAttribute('id');
   const errorElement = document.getElementById(`${id}-error`);
-  input.addEventListener('input', (event) =>
-    handleChange(event, id, errorElement)
-  );
+  input.addEventListener('input', (event) => {
+    handleChange(event, id, errorElement);
+  });
 }
 
 function handleChange(event, id, errorElement) {
@@ -19,39 +20,48 @@ function handleChange(event, id, errorElement) {
 
 function validateInput(id, value) {
   let error = '';
+
   switch (id) {
     case 'name':
       if (!value) error = 'Name cannot be blank.';
       break;
+
     case 'email':
       if (!value.includes('@cmu.ac.th')) error = 'Invalid CMU email.';
       break;
+
     case 'password':
       if (value.length < 6) error = 'Password must be at least 6 characters.';
       break;
+
     case 'password-confirm':
       const password = document.getElementById('password').value;
       if (value !== password) error = 'Passwords do not match.';
       break;
+
     default:
-      console.log('Unknwon id');
+      console.log('Unknown Id');
   }
   return error;
 }
 
 function validateAllInputs() {
   let pass = true;
+
   for (const input of inputArray) {
     const id = input.getAttribute('id');
     const value = input.value;
     pass = pass && validateInput(id, value) === '';
   }
+
   btnElement.disabled = !pass;
 }
 
 function logSubmit(event) {
   event.preventDefault();
+
   let output = '';
+
   const formElements = event.target;
   for (const formElement of formElements) {
     const id = formElement.getAttribute('id');
@@ -65,5 +75,4 @@ function logSubmit(event) {
   for (const input of inputArray) {
     input.value = '';
   }
-  btn.disabled = true;
 }
