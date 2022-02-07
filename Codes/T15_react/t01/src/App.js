c345d78037bd82ea7ea1b588eac46580e8b099b1
Nirand <56184504+nnnpooh@users.noreply.github.com>
2022-02-07 23:08:26 +0700

    update

--- Codes/T15_react/t01/src/App.js
+++ Codes/T15_react/t01/src/App.js
@@ -16,24 +16,6 @@ function App() {
       <p>{title1}</p>
       {title2}
       {title3}
-
-      <h1>Counter</h1>
-      <p>Count: {count}</p>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        -
-      </button>
-
       <h1>Not Working</h1>
       <p>Count: {count2}</p>
       <button
@@ -48,6 +30,23 @@ function App() {
         onClick={() => {
           count2 = count2 - 1;
           console.log(count2);
+        }}
+      >
+        -
+      </button>
+
+      <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
         }}
       >
         -
